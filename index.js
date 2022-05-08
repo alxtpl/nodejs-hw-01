37f809239fd3711b4a0e7c4e8af9663ed9b248a5
Alexander Teplechuk <79643383+alxtpl@users.noreply.github.com>
2022-05-08 17:42:46 +0200

    test com

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 console.log('heloooooooooooooooooooooooooooooooooooooooo');
+console.log('heloooooooooooooooooooooooooooooooooooooooo');
+console.log('heloooooooooooooooooooooooooooooooooooooooo');
+console.log('heloooooooooooooooooooooooooooooooooooooooo');
